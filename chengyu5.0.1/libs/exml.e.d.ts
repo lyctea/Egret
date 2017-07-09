be569fd6c5e75b25dbf1824d3202b2707fbcb968
luoyecong <luoyec@talkmoney.cn>
2017-07-10 01:11:39 +0800

    游戏界面构建和设计

--- chengyu5.0.1/libs/exml.e.d.ts
+++ chengyu5.0.1/libs/exml.e.d.ts
@@ -2,7 +2,9 @@ declare class LevelIconSkin extends eui.Skin{
 }
 declare class NewFile extends eui.Skin{
 }
+declare class SceneGameSkin extends eui.Skin{
+}
 declare class SceneLevelsSkin extends eui.Skin{
 }
-declare class test extends eui.Skin{
+declare class WordSkin extends eui.Skin{
 }
