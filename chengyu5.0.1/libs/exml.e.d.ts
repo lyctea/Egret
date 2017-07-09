6c2160b6d7895113d661582e6e74215c9ebf5f43
luoyecong <luoyec@talkmoney.cn>
2017-07-09 23:50:50 +0800

    基于5.0.1引擎的成语大挑战

---
+++ chengyu5.0.1/libs/exml.e.d.ts
@@ -0,0 +1,8 @@
+declare class LevelIconSkin extends eui.Skin{
+}
+declare class NewFile extends eui.Skin{
+}
+declare class SceneLevelsSkin extends eui.Skin{
+}
+declare class test extends eui.Skin{
+}
--- chengyudatiaozhan/libs/exml.e.d.ts
+++
@@ -1,6 +0,0 @@
-declare class LevelIconSkin extends eui.Skin{
-}
-declare class NewFile extends eui.Skin{
-}
-declare class SceneLevelsSkin extends eui.Skin{
-}
